6ff4bfb40d665a8ea8ecfd9cb2ff76ef25468edf
srsuti617 <srustigourani617@gmail.com>
2021-02-17 12:12:13 +0000

    finding odd or even

--- Problem_3.c
+++ Problem_3.c
@@ -2,13 +2,15 @@
 
 #include <stdio.h>
 int main() {
-    int number;
+    int num;
     printf("Enter an integer: ");
-    scanf("%d", &number);
+    scanf("%d", &num);
 
     // Write your code here
-    
-    
+    if (num%2==0)
+    printf("%d is Even\n",num);
+    else
+    printf("%d is Odd\n",num);    
    
     return 0;
 }
